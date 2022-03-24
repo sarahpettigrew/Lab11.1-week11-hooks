021689d9f9bd8b1fc4c9d7d12d30110e18bd3028
Sarah Pettigrew <sarahepettigrew@outlook.com>
2022-03-23 17:40:57 -0700

    changed case of entered value and searched values to ignore case

--- src/App.js
+++ src/App.js
@@ -1,72 +1,70 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
   const stories = [
     {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState("React");
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearch = event => {
-    setSearchTerm(event.target.value)
-  }
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-  const searchedStories = stories.filter( function (story) {
-    return story.title.includes(searchTerm)
-  })
-
+  const searchedStories = stories.filter(function (story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
       <Search onSearch={handleSearch} searchTerm={searchTerm} />.
-
       <hr />
-
       <List list={searchedStories} />
     </div>
   );
 };
 
 const Search = (props) => {
-
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" value={props.searchTerm} onChange={props.onSearch} />
+      <input
+        id="search"
+        type="text"
+        value={props.searchTerm}
+        onChange={props.onSearch}
+      />
 
       <p>
         Searching for <strong>{props.searchTerm}</strong>.
       </p>
-
     </div>
-  )
+  );
+};
 
-}
-
-const List = props =>
-  props.list.map(item => (
+const List = (props) =>
+  props.list.map((item) => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
